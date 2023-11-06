cc6aedd3672c1f3a3c8e65ed5a43d3f562f64abb
Prabaharan-nagappan <prabaharan.nagappan@gmail.com>
2023-11-06 11:49:41 +0000

    added simple project

---
+++ myproject/myapp/models.py
@@ -0,0 +1,8 @@
+# Create your models here.
+# myapp/models.py
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
---
+++ myproject/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+# myapp/views.py
+from rest_framework import generics
+from .models import Item
+from .serializers import ItemSerializer
+
+class ItemListCreateView(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+class ItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
